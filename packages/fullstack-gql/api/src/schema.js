8012b45ef8e46a03e851d0893877e6386544e577
Isaac Ramirez <iramirezc@live.com.mx>
2021-02-21 14:37:48 -0600

    Queries & Resolvers: - solved Query Types & Resolvers exercise

--- packages/fullstack-gql/api/src/schema.js
+++ packages/fullstack-gql/api/src/schema.js
@@ -1,8 +1,25 @@
-const { gql } = require("apollo-server");
+const gql = require("graphql-tag");
 
 /**
  * Type Definitions for our Schema using the SDL.
  */
-const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+    id: String!
+    username: String!
+  }
+
+  type Pet {
+    id: String!
+    createdAt: String!
+    name: String!
+    type: String
+  }
+
+  type Query {
+    user: User!
+    pets: [Pet]!
+  }
+`;
 
 module.exports = typeDefs;
